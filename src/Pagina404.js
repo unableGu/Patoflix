de33664eea84d2894500db16373dfc7e05133eac
Gustavo Triaquim <gustavotriaquim6@gmail.com>
2024-07-28 13:12:19 -0300

    Commit 2

---
+++ src/Pagina404.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Pagina404() {
+  return (
+    <div>Pagina 404</div>
+  );
+}
+
+export default Pagina404;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './pages/Home/App';
+import Pagina404 from './Pagina404';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} exact />
+      <Route path="/cadastro/video" element={<CadastroVideo />} />
+      <Route path="/cadastro/categoria" element={<CadastroCategoria />} />
+      <Route path="*" element={<Pagina404 />} />
+    </Routes>
+  </BrowserRouter>
 );
